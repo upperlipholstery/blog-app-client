35f386bf37ec4407ddbe77f333760bc4e81c47f9
Kane <Pperson599@gmail.com>
2020-04-22 12:19:14 -0400

    View Posts
    - created button to show all the posts
    - fixed small pieces of lat committed
    
    Close #19

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,7 @@
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
+const postEvents = require('./posts/events')
 const authEvents = require('./auth/events')
 // use require without a reference to ensure a file is bundled
 // require('./example')
@@ -11,4 +12,6 @@ $(() => {
   $('#sign-in-btn').on('click', authEvents.onSignIn)
   $('.change-password-button').on('click', authEvents.onChangePassword)
   $('#sign-out-btn').on('click', authEvents.onSignOut)
+
+  $('#view-posts-btn').on('click', postEvents.onViewPosts)
 })
