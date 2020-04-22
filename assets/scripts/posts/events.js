71ad10da5e13dd978102ee0796121a1a8b48b86e
Kane <Pperson599@gmail.com>
2020-04-22 15:45:52 -0400

    Fixed merge conflicts

--- assets/scripts/posts/events.js
+++ assets/scripts/posts/events.js
@@ -68,6 +68,7 @@ function onUpdatePractice (event) {
 function onCreatePost (event) {
   event.preventDefault()
   const data = getFormFields(event.target)
+  console.log(data)
   api.createPost(data)
     .then(ui.createPostSuccess)
     .catch(ui.createPostFailure)
