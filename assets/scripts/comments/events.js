41d2151c071f625d88a6dcea01721225cfdc0cf8
Kane <Pperson599@gmail.com>
2020-04-24 00:45:44 -0400

    Update comments
    - Created update comment feature
    
    Close #39

--- assets/scripts/comments/events.js
+++ assets/scripts/comments/events.js
@@ -24,31 +24,28 @@ function onUpdateComment (event) {
 }
 
 function populateComment (data) {
-  const updateCommentHtml = updateCommentTemplate({post: data.post})
-  console.log(updateCommentHtml)
-  $(`.comment-body[data-id=${store.updateCommentId}]`)[0].html(updateCommentHtml)
-  // const updateData = getFormFields($('#edit-post')[0])
-  // api.updateComment(updateData, store.updateCommentId)
-  //   .then(ui.updateCommentsSuccess)
-  //   .catch(ui.updateCommentsFailure)
-}
-
-function cancelUpdateComment (event) {
-  event.preventDefault()
-  store.updateItemId = undefined
+  const updateCommentHtml = updateCommentTemplate({comment: data.comment[0]})
+  $($(`.comment-body[data-id=${store.updateCommentId}]`)[0]).html(updateCommentHtml)
 }
 
 function confirmUpdateComment (event) {
   event.preventDefault()
-  store.updateCommentId = $(event.target).data('id')
   console.log(store.updateCommentId)
-  api.showComment(store.updateCommentId)
-    .then(ui.selectUpdateCommentsSuccess)
+  console.log($(`.edit-comment-form[data-id=${store.updateCommentId}]`)[0])
+  const data = getFormFields($(`.edit-comment-form[data-id=${store.updateCommentId}]`)[0])
+  data.comment.postId = store.viewItemId
+  api.updateComment(data, store.updateCommentId)
+    .then(ui.confirmUpdateCommentSuccess)
     .catch(ui.updateCommentsFailure)
 }
 
+function cancelUpdateComment (event) {
+  event.preventDefault()
+  store.updateItemId = null
+}
 
 module.exports = {
   onCreateComment,
-  onUpdateComment
+  onUpdateComment,
+  confirmUpdateComment
 }
