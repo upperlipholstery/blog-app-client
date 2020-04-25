2b0032175a56c37c770ac02de7f411c58178c845
Kane <Pperson599@gmail.com>
2020-04-25 19:08:49 -0400

    Ui file refactor

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -1,68 +1,10 @@
 'use strict'
 
-const store = require('./store')
-const api = require('./auth/api')
-
-// AUTHENTICATION
-
-function signUpSuccess (data) {
-  console.log(data)
-  store.user = data.user
-  // api.signIn(data)
-  //   .then(signInSuccess)
-  //   .catch(signInFailure)
-  console.log('signUp working')
-}
-
-function signInSuccess (data) {
-  store.user = data.user
-  $('#regsidebar').removeClass('hidden')
-  $('#unregsidebar').addClass('hidden')
-}
-
-function signOutSuccess () {
-  console.log('sign out is working')
-  $('#unregsidebar').removeClass('hidden')
-  $('#regsidebar').addClass('hidden')
-  $('#create-comment-menu').addClass('hidden')
-  $('#create-post-menu').addClass('hidden')
-  $('#content').html('')
-  store.user = undefined
-}
-
 function showWritePost () {
   $('#post-content').addClass('hidden')
   $('#create-post-menu').removeClass('hidden')
 }
 
-function signUpFailure () {
-  console.log('signUp failed')
-}
-
-function signInFailure () {
-  console.log('sign in failed')
-}
-
-function signOutFailure () {
-  console.log('sign out failed')
-}
-
-function changePasswordSuccess () {
-  console.log('change password working')
-}
-
-function changePasswordFailure () {
-  console.log('change password failed')
-}
-
 module.exports = {
-  signUpSuccess,
-  signInSuccess,
-  signInFailure,
-  signUpFailure,
-  changePasswordSuccess,
-  changePasswordFailure,
-  signOutSuccess,
-  signOutFailure,
   showWritePost
 }
